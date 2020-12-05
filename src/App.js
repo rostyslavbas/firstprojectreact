99257a88d507c1641e6cb15ee00476bb3e221dea
rostyslavbas <rostyslavbas@gmail.com>
2020-12-05 10:37:34 +0200

    /

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
-import {cars} from "./Source/source";
-import Cars from "./Data/cars";
+import AllPosts from './components/all-posts/AllPosts';
+
 
 class App extends Component {
+
+
     render() {
         return (
             <div>
-                {cars.map((cars, index) => <Cars item = {cars} key = {index}/>)}
+                <AllPosts/>
             </div>
         );
     }
